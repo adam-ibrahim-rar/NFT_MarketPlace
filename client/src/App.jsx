908df4eecbc939cfd3ce80c34fc25a3d93002130
Adam Ibrahim <adamdomadoma@gmail.com>
2026-07-16 01:37:52 +0300

    creat pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,19 @@
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+import SingUp from "./Pages/SingUp";
+import Ranking from "./Pages/Ranking";
+import MarketPlace from "./Pages/MarketPlace";
+import NotFound from "./Pages/NotFound";
 export default function App() {
   return (
-    <div>
-      hi
-    </div>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/SingUp" element={<SingUp />} />
+        <Route path="/Ranking" element={<Ranking />} />
+        <Route path="/MarketPlace" element={<MarketPlace />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
